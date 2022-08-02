10a591cb588f013b50c91012dfc0e6e11fee74ea
AdileneRodri <86806392+AdileneRodri@users.noreply.github.com>
2022-08-01 17:58:16 -0700

    changed order of artist routes

--- backend/routes/api/artists.js
+++ backend/routes/api/artists.js
@@ -6,6 +6,72 @@ const { Op } = require("sequelize");
 const { User, Songs, Albums, Playlists } = require('../../db/models');
 
 
+
+
+// Get playlist from artist id
+router.get('/:artistId/playlists',
+async (req, res, next) => {
+    const { artistId } = req.params;
+    const artist = await User.findByPk(artistId);
+    
+    if(!artist){
+        res.status(404);
+        return res.json({
+            "message": "Artist does not exist",
+            "statusCode": 404
+        });
+    }
+    const artistPlaylists = await Playlists.findAll({
+        where: {
+            userId: artistId
+        }
+    });
+    return res.json({"Playlists": artistPlaylists});
+});
+
+
+// Get all albums from artist by id
+router.get('/:artistId/albums',
+async (req, res, next) => {
+    const { artistId } = req.params;
+    const artist = await User.findByPk(artistId);
+    
+    if(!artist){
+        res.status(404);
+        return res.json({
+            "message": "Artist does not exist",
+            "statusCode": 404
+        });
+    }
+    const artistAlbums = await Albums.findAll({
+        where: {
+            userId: artistId
+        }
+    });
+    return res.json({"Albums": artistAlbums});
+});
+
+// Get all songs from artist based on id
+router.get('/:artistId/songs',
+async (req, res, next) => {
+    const { artistId } = req.params;
+    const artist = await User.findByPk(artistId);
+    
+    if(!artist){
+        res.status(404);
+        return res.json({
+            "message": "Artist does not exist",
+            "statusCode": 404
+        });
+    }
+    const artistSongs = await Songs.findAll({
+        where: {
+            userId: artistId
+        }
+    });
+    return res.json({"Songs": artistSongs});
+});
+
 // Get details of artist by id
 router.get('/:artistId', async (req, res, next) => {
     const { artistId } = req.params;
@@ -37,70 +103,4 @@ router.get('/:artistId', async (req, res, next) => {
 
     return res.json(artist);
 });
-
-
-// Get playlist from artist id
-router.get('/:artistId/playlists',
-    async (req, res, next) => {
-        const { artistId } = req.params;
-        const artist = await User.findByPk(artistId);
-
-        if(!artist){
-            res.status(404);
-            return res.json({
-                "message": "Artist does not exist",
-                "statusCode": 404
-            });
-        }
-        const artistPlaylists = await Playlists.findAll({
-            where: {
-                userId: artistId
-            }
-        });
-        return res.json({"Playlists": artistPlaylists});
-});
-
-
-// Get all albums from artist by id
-router.get('/:artistId/albums',
-    async (req, res, next) => {
-        const { artistId } = req.params;
-        const artist = await User.findByPk(artistId);
-
-        if(!artist){
-            res.status(404);
-            return res.json({
-                "message": "Artist does not exist",
-                "statusCode": 404
-            });
-        }
-        const artistAlbums = await Albums.findAll({
-            where: {
-                userId: artistId
-            }
-        });
-        return res.json({"Albums": artistAlbums});
-});
-
-// Get all songs from artist based on id
-router.get('/:artistId/songs',
-    async (req, res, next) => {
-        const { artistId } = req.params;
-        const artist = await User.findByPk(artistId);
-
-        if(!artist){
-            res.status(404);
-            return res.json({
-                "message": "Artist does not exist",
-                "statusCode": 404
-            });
-        }
-        const artistSongs = await Songs.findAll({
-            where: {
-                userId: artistId
-            }
-        });
-        return res.json({"Songs": artistSongs});
-});
-
 module.exports = router;
