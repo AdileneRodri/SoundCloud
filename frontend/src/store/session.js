5f8f1a166144bf9242b7ef68d157332927fd683c
AdileneRodri <86806392+AdileneRodri@users.noreply.github.com>
2022-09-26 21:21:04 -0700

    Restore session user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,6 +33,13 @@ export const login = (user) => async (dispatch) => {
   return res;
 };
 
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const sessionReducer = (state = initialState, action) => {
   let newState;
 
